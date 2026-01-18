3434ea5b819c2546f2a93b5280963b4c030d5fd2
Lucas <lucas.tesseron@gmail.com>
2026-01-18 13:15:20 +0700

    world 2,3 straight segments

--- Assets/Scripts/WorldGeneration/BiomesData.cs
+++ Assets/Scripts/WorldGeneration/BiomesData.cs
@@ -1,9 +1,8 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class BiomesData : Singleton<BiomesData>, IInitializable
 {
